5e4ea238a1a67e3d0d8e5298b3e89c86cd4399f1
Bilgin Ibryam <bibryam@gmail.com>
2018-07-16 12:18:51 +0100

    Removed unsused storage contract

--- contracts/Storage.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.23;
-contract Storage {
-    uint pos0;
-    function Storage() {
-        pos0 = 5;
-    }
-}
--- migrations/3_deploy_storage.js
+++
@@ -1,5 +0,0 @@
-var Storage = artifacts.require("./Storage.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(Storage);
-};
